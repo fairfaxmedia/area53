4f7b8855ada66992b63e8aeb1cc3d60446eeea92
Chris Knowles <chris.knowles@bitgamelabs.com>
2016-11-03 18:53:04 +0800

    Various fixes to new Ingress function
    - Ensure active_support extensions are loaded for non-rails as
    currently it fails with `undefined method `present?' for nil:NilClass
    (NoMethodError)` out of the box
    - Start some tests for the main app logic
    - Add new Ingress details to readme
    - Ensure the tests work without ffx/container_logging

--- area53.rb
+++ area53.rb
@@ -1,22 +1,27 @@
-#!/usr/bin/env ruby
 require_relative 'watchers/service_watcher'
 require_relative 'watchers/ingress_watcher'
 require_relative 'kube_client'
 require_relative 'route53_client'
+require 'active_support'
+require 'active_support/core_ext/object'
 
-logger = begin
-  require 'ffx/container_logging'
-  Ffx::ContainerLogging::ConfigurationService.new(app_name: 'area-53').logger
-rescue LoadError
-  logger = Logger.new(STDOUT)
-  logger.info(status: 'setup_log', msg: 'ffx/container_logging not present, using stdout')
-  logger
+class Area53
+  def run
+    logger = begin
+      require 'ffx/container_logging'
+      Ffx::ContainerLogging::ConfigurationService.new(app_name: 'area-53').logger
+    rescue LoadError
+      logger = Logger.new(STDOUT)
+      logger.info(status: 'setup_log', msg: 'ffx/container_logging not present, using stdout')
+      logger
+    end
+
+    route53_client = Route53Client.new(logger, ENV['HOSTED_ZONE_ID'])
+
+    service_thread = Thread.new { ServiceWatcher.new(logger, route53_client).run }
+    ingress_thread = Thread.new { IngressWatcher.new(logger, route53_client, ENV['ELB']).run } if ENV['ELB'].present?
+
+    service_thread.join
+    ingress_thread.join unless ingress_thread.nil?
+  end
 end
-
-route53_client = Route53Client.new(logger, ENV['HOSTED_ZONE_ID'])
-
-service_thread = Thread.new { ServiceWatcher.new(logger, route53_client).run }
-ingress_thread = Thread.new { IngressWatcher.new(logger, route53_client, ENV['ELB']).run } if ENV['ELB'].present?
-
-service_thread.join
-ingress_thread.join unless ingress_thread.nil?
